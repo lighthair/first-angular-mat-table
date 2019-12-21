5db26358508f08f5c90557d214e404aaf8890a3b
lighthair <lichtes.haar@gmx.de>
2019-12-21 14:20:05 +0100

    just try updating angular

--- src/app/billingitem/billingitem-datasource.ts
+++ src/app/billingitem/billingitem-datasource.ts
@@ -1,16 +1,15 @@
-import {CollectionViewer, DataSource, } from "@angular/cdk/collections";
-import {BillingItem} from "./billingitem";
-import {BillingItemService} from "./billingitem.service";
-import {BehaviorSubject, Observable, of} from 'rxjs';
+import { CollectionViewer, DataSource, } from '@angular/cdk/collections';
+import { BillingItem } from './billingitem';
+import { BillingItemService } from './billingitem.service';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
-export class BillingitemDatasource implements DataSource<BillingItem>{
-   
+export class BillingitemDatasource implements DataSource<BillingItem> {
     private billingSubject = new BehaviorSubject<BillingItem[]>([]);
     private loadingSubject = new BehaviorSubject<boolean>(false);
 
     public loading$ = this.loadingSubject.asObservable();
 
-    constructor(private billingItemService: BillingItemService) {}
+    constructor(private billingItemService: BillingItemService) { }
 
     connect(collectionViewer: CollectionViewer): Observable<BillingItem[]> {
         return this.billingSubject.asObservable();
@@ -22,15 +21,15 @@ export class BillingitemDatasource implements DataSource<BillingItem>{
     }
 
     loadLessons(courseId: string, filter = '',
-                sortDirection = 'asc', pageIndex = 0, pageSize = 3) {
+        sortDirection = 'asc', pageIndex = 0, pageSize = 3) {
 
         this.loadingSubject.next(true);
 
         this.billingItemService.findBill(courseId, filter, sortDirection,
             pageIndex, pageSize).pipe(
-            () => of([]),
-            () => of([])
-        )
-        .subscribe(lessons => this.billingSubject.next(lessons));
-    }    
+                () => of([]),
+                () => of([])
+            )
+            .subscribe(lessons => this.billingSubject.next(lessons));
+    }
 }
