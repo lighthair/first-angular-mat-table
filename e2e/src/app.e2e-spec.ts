409a3e9e408a2deb96f939ecfdc691e6982c9414
lighthair <lichtes.haar@gmx.de>
2020-01-18 18:00:13 +0100

    test (testcafe): add tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,10 +1,23 @@
 import { Selector } from 'testcafe';
+import { AngularJSSelector, AngularSelector } from 'testcafe-angular-selectors';
 
 fixture('Smoke test')
   .page('http://localhost:4200');
 
 test('Title should be BFZ Rechnung', async t => {
-  const articleHeader = await Selector('title');
+  const articleHeader = Selector('title');
   await t
     .expect(articleHeader.innerText).eql('BFZ Rechnung');
+});
+
+test('There should be an Add Button', async t => {
+  const addBtn = AngularSelector('button');
+  await t
+    .expect(addBtn.innerText).eql('Add');
+});
+
+test('There should be an mat table', async t => {
+  const table = AngularSelector('mat-table');
+  await t
+    .expect(table.childElementCount).eql(1);
 });
